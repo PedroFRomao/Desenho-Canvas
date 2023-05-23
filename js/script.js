31d06ce812b02a59d8d137b0d80252e2d0f8370b
PedroFRomao <pedroferraz.romao@gmail.com>
2023-05-23 11:34:17 -0300

    teste 3

--- js/script.js
+++ js/script.js
@@ -1,36 +1,32 @@
-//titulo
+//base
 var canvas = document.getElementById("UgCanvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle="#227bb5";
-ctx.font = "italic 40pt arial";
-ctx.fillText("Teste de elelmentos do canva", 100, 100);
 
-//retangulo
-ctx.fillStyle="#227bb5";
-ctx.fillRect(50,150,250,150);
+//ceu
+ctx.fillStyle="#2e2e2e";
+ctx.fillRect(0,0,1080,720);
 
-//circulo
+//chao
+ctx.fillStyle="#e0e0e0";
+ctx.fillRect(0,400,1080,320);
+
+/*//circulo
 ctx.fillStyle="#227bb5";
 ctx.beginPath();
 ctx.arc(450,250,80,100,Math.PI*2,true);
 ctx.closePath();
-ctx.fill();
+ctx.fill();*/
 
 //curva
 ctx.beginPath();
-ctx.moveTo(600,350);
-ctx.quadraticCurveTo(800,170,770,320);
+ctx.moveTo(0,120);
+ctx.quadraticCurveTo(100,-1,420,0);
+ctx.quadraticCurveTo(500,20,650,150);
+ctx.quadraticCurveTo(1500,800,1000,100);
 ctx.stroke();
 
-//linha
+/*//linha
 ctx.moveTo(600,250);
 ctx.lineTo(650,150);
 ctx.lineTo(750,200);
-ctx.stroke();
-
-//imagem
-var img=new Image();
-img.onload = function(){
-ctx.drawImage(img,150,450);
-};
-img.src="img/img.png"; 
+ctx.stroke();*/
